83850cda97aece8b2d8004e356cd48f938c16c63
JR <180821520+mscbuild@users.noreply.github.com>
2025-05-10 15:36:36 +0300

    Update employeeRoutes.js

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const { createEmployee, getEmployees } = require('../controllers/employeeController');
+const router = express.Router();
+
+router.post('/employees', createEmployee);
+router.get('/employees', getEmployees);
+
+module.exports = router;
 
