7b6df8ee2b7e3033fbb719bec7b8b130adf37857
JR <180821520+mscbuild@users.noreply.github.com>
2025-05-10 15:31:58 +0300

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  role: { type: String, required: true },
+  department: { type: String, required: true },
+  contact: { type: String, required: true },
+  salary: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
 
