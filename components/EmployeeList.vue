027274c42d5ee0a0237d2fcb135b4a2f6c813804
JR <180821520+mscbuild@users.noreply.github.com>
2025-05-10 15:42:35 +0300

    Create EmployeeList.vue

---
+++ components/EmployeeList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Employees</h2>
+    <ul>
+      <li v-for="emp in employees" :key="emp._id">
+        {{ emp.name }} — {{ emp.role }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      employees: [],
+    };
+  },
+  created() {
+    axios.get('http://localhost:5000/api/employees')
+      .then(response => {
+        this.employees = response.data;
+      });
+  },
+};
+</script>
